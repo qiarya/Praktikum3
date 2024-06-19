9c4c7c9f1e7d098a0d37481eb09c48f582c6b459
qiarya <168283092+qiarya@users.noreply.github.com>
2024-06-19 21:50:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<center>Gender: ".$_POST['Gender']."</center><br>";
+echo "<center>Age: ".$_POST['Age']."</center><br>";
+echo "<center> football club option: ".$_POST['FC']."</center><br>";
+?>
