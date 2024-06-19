10f527e6423b3d9bc83c6087a97b949893aba831
qiarya <168283092+qiarya@users.noreply.github.com>
2024-06-19 22:07:32 +0700

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<center>Gender: ".$_POST['Gender']."</center><br>";
-echo "<center>Age: ".$_POST['Age']."</center><br>";
-echo "<center> football club option: ".$_POST['FC']."</center><br>";
-?>
